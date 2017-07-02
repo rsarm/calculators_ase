ab9a9a3e41b012781c2eab9bd0e2fd5056dad38e
rsarm <rsarmientoperez@gmail.com>
2017-07-02 08:46:32 +0200

    Moving to new implementation

--- obc.py
+++ obc.py
@@ -1,5 +1,8 @@
 """This module defines an ASE calculator 'obc'.
 
+This calculator uses the force fields implemented in OpenBabel
+to compute energies and numerical forces from them.
+
 """
 
 import numpy as np
@@ -11,7 +14,51 @@ import pybel
 
 
 
+
+
+
+
+
+def ase2xyz(atoms):
+    """Return a string containing the xyz file.
+
+    input:
+
+    * m :: ase Atoms object.
+
+    """
+
+    N=atoms.get_number_of_atoms()
+
+    xyz=str(N)+'\nMol  0.0  0.0\n'
+
+    # Loop first over the first N-1 atoms.
+    # The last line of the xyz file is added after the loop
+    # to avoid the an empty line at the end of the file.
+    for i in range(N-1):
+        xyz+=atoms.get_chemical_symbols()[i]+ ' '.join("%16.8f" % n for n in atoms.positions[i])+'\n'
+
+    return xyz + atoms.get_chemical_symbols()[N-1]+ ' '.join("%16.8f" % n for n in atoms.positions[N-1])
+
+
+
+
+
+#In [120]: for i,a in enumerate(pybel.ob.OBMolAtomIter(m.OBMol)):
+#     ...:     a.SetVector(i*3*0.2,0.1,0.0)
+#
+#In [121]: ff.SetCoordinates(m.OBMol)
+#Out[121]: True
+#
+#In [122]: ff.Energy()
+#Out[122]: 868.463711181507
+
+
+
+
 def ob_forcefield(atoms,ffname):
+    """Sets up the OB force field."""
+
     #Setting up openbabel
     obmol = pybel.ob.OBMol()
     obconversion = pybel.ob.OBConversion()
@@ -21,10 +68,10 @@ def ob_forcefield(atoms,ffname):
     obconversion.ReadString(obmol,ase2xyz(atoms))
 
 
-    # Get the ff 
+    # Get the FF
     ff=pybel.ob.OBForceField.FindForceField(ffname)
-    #ff.SetLogLevel(pybel.ob.OBFF_LOGLVL_HIGH) 
-    #ff.SetLogToStdErr() 
+    #ff.SetLogLevel(pybel.ob.OBFF_LOGLVL_HIGH)
+    #ff.SetLogToStdErr()
     ff.Setup(obmol)
 
     return (ff,obmol)
@@ -33,33 +80,27 @@ def ob_forcefield(atoms,ffname):
 
 
 
-def ase2xyz(m):
-    """Return a string containing the xyz file.
 
-    input:
+def e_ff(atoms, obmol, obff, ffunitfactor):
+    """Potential energy computed with a force field of openbabel.
 
-    * m :: ase Atoms object.
-
+    * atoms :: ase Atoms object
     """
 
-    N=m.get_number_of_atoms()
+    for i,a in enumerate(pybel.ob.OBMolAtomIter(obmol)):
+        a.SetVector(atoms.positions[i,0],
+                    atoms.positions[i,1],
+                    atoms.positions[i,2])
 
-    xyz=str(N)+'\nMol  0.0  0.0\n'
+    obff.SetCoordinates(obmol)
 
-    # Loop first over the first N-1 atoms.
-    # The last line of the xyz file is added after the loop
-    # to avoid the an empty line at the end of the file.
-    for i in range(N-1):
-        xyz+=m.get_chemical_symbols()[i]+ ' '.join("%16.8f" % n for n in m.positions[i])+'\n'
-
-    return xyz + m.get_chemical_symbols()[N-1]+ ' '.join("%16.8f" % n for n in m.positions[N-1])
+    # The factor 4.1572299 is R/2 (half the gas constant)
+    return obff.Energy() * units.kcal / units.mol * ffunitfactor # Result in eV
 
 
 
 
-
-
-def e_ff(atoms,ffname,ffunitfactor):
+def _e_ff(atoms,ffname,ffunitfactor):
     """Potential energy computed with a force field of openbabel.
 
     * atoms :: ase Atoms object
@@ -74,7 +115,8 @@ def e_ff(atoms,ffname,ffunitfactor):
 
 
 
-def numeric_force(atoms, a, i, ffname,ffunitfactor, d=0.0001):
+
+def _numeric_force(atoms, a, i, ffname,ffunitfactor, d=0.0001):
     """Evaluate force along i'th axis on a'th atom using finite difference.
 
     This will trigger two calls to get_potential_energy(), with atom a moved
@@ -93,9 +135,28 @@ def numeric_force(atoms, a, i, ffname,ffunitfactor, d=0.0001):
     return (eminus - eplus) / (2 * d)
 
 
+def numeric_force(atoms, a, i, obmol, obff, ffunitfactor, d=0.0001):
+    """Evaluate force along i'th axis on a'th atom using finite difference.
+
+    This will trigger two calls to get_potential_energy(), with atom a moved
+    plus/minus d in the i'th axial direction, respectively.
+
+    Based in ase.calculators.test
+    """
+    p0 = atoms.positions[a, i]
+    atoms.positions[a, i] += d
+    eplus  = e_ff(atoms, obmol, obff, ffunitfactor)
+    atoms.positions[a, i] -= 2 * d
+    eminus = e_ff(atoms, obmol, obff, ffunitfactor)
+    atoms.positions[a, i] = p0
 
 
-def f_ff(atoms,ffname,ffunitfactor):
+    return (eminus - eplus) / (2 * d)
+
+
+
+
+def _f_ff(atoms,ffname,ffunitfactor):
     """Evaluate numeric forces.
 
     Args:
@@ -104,8 +165,21 @@ def f_ff(atoms,ffname,ffunitfactor):
 
     return np.array([[numeric_force(atoms, a, i, ffname,ffunitfactor)
             for i in range(3)] for a in range(len(atoms))])
+#
 
 
+
+
+def f_ff(atoms, obmol, obff, ffunitfactor):
+    """Evaluate numeric forces.
+
+    Args:
+    * atoms     :: ASE Atoms object
+    """
+
+    return np.array([[numeric_force(atoms, a, i, obmol, obff, ffunitfactor, d=0.0001)
+            for i in range(3)] for a in range(len(atoms))])
+#
 
 
 
@@ -150,6 +224,9 @@ class OBC(Calculator):
     def __init__(self, **kwargs):
         Calculator.__init__(self, **kwargs)
 
+        self.obff, self.obmol = ob_forcefield(kwargs['atoms'],kwargs['ff'])
+
+
 
 
 
@@ -174,8 +251,11 @@ class OBC(Calculator):
         #alphas  = self.parameters.alphas
         #gamma   = self.parameters.gamma
 
-        self.results['energy'] = e_ff(self.atoms,ffname,ffunitfactor)
-        self.results['forces'] = f_ff(self.atoms,ffname,ffunitfactor)
+        #self.results['energy'] = e_ff(self.atoms,ffname,ffunitfactor)
+        #self.results['forces'] = f_ff(self.atoms,ffname,ffunitfactor)
+
+        self.results['energy'] = e_ff(self.atoms, self.obmol, self.obff, ffunitfactor)
+        self.results['forces'] = f_ff(self.atoms, self.obmol, self.obff, ffunitfactor)
 
 
 
@@ -190,7 +270,7 @@ class OBC(Calculator):
 
         # The highest is 3 (pybel.ob.OBFF_LOGLVL_HIGH)
         obff.SetLogLevel(loglevel)
-        obff.SetLogToStdErr() 
+        obff.SetLogToStdErr()
 
         # Optimization algorithms
         if algo.lower()=='cg' or algo.lower()=='conjugategradients':
@@ -198,27 +278,9 @@ class OBC(Calculator):
         if algo.lower()=='sd' or algo.lower()=='steepestdescent':
             obff.SteepestDescent(nsteps)
         #
-        obff.GetCoordinates(obmol)
+        #obff.GetCoordinates(obmol)
+        obff.UpdateCoordinates(obmol)
 
         opt_positions=[[a.x(),a.y(),a.z()] for a in pybel.ob.OBMolAtomIter(obmol)]
 
         atoms.set_positions(np.array(opt_positions))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
