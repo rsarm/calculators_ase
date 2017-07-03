a6d4a5663fe30a33ebd528c820ed76506526bb1d
rsarm <rsarmientoperez@gmail.com>
2017-07-03 18:16:46 +0200

    test external reference

---
+++ tests/test_external_reference/gs.py
@@ -0,0 +1,27 @@
+
+import sys
+from ase.io              import read
+from ase.calculators.obc import OBC
+from ase                 import units
+
+
+mol=read(sys.argv[1])
+
+
+calc=OBC() #['gaff','ghemical','mmff94','mmff94s','uff']
+
+#calc.parameters['atoms']  = mol
+calc.parameters['ff']     = 'mmff94'
+calc.parameters['extref'] = 'AMPTRB10.mol2'
+calc.setup_ff()
+
+mol.set_calculator(calc)
+
+
+# Relaxing geometry with autoopt()
+calc.autoopt(mol)
+
+print '\nEnergy by terms (kcal/mol):-----------------'
+e_terms_dict=calc.get_potential_energy_terms(mol,energy_unit=units.kcal/units.mol)
+
+print ''
