67a78ee56a9e18a88f97fde6a4c96e4f3a9b9016
rsarm <rsarmientoperez@gmail.com>
2017-07-03 16:30:44 +0200

    update mmff94 example

--- tests/test_mmff94/run.sh
+++ tests/test_mmff94/run.sh
@@ -1,4 +1,11 @@
 
+#      AGLYSL01       26.87431        26.87431               AGLYSL01            26.874216    26.874213
+echo  "            Name                 E1_ref       E2_ref     Name           E1_calc         Er_calc"
+#            AGLYSL01            26.874216    26.874213   AGLYSL01       26.87431        26.87431
+
 for i in `less energies.dat | awk '{print $1}'` ; do
-    python gs.py ${i}.xyz;
+    calc=`python gs.py ${i}.xyz`
+    echo -n "$calc   "
+
+    grep $i energies.dat
 done
